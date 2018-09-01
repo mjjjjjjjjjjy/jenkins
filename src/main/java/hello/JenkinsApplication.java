a6bba2f5bc42c4b3ab493515380e17cad18db52d
mjjjjjjjjjjy <1194229162@qq.com>
2018-09-01 20:51:47 +0800

    jj

--- src/main/java/hello/JenkinsApplication.java
+++ src/main/java/hello/JenkinsApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
 @Controller
@@ -14,7 +15,8 @@ public class JenkinsApplication {
 	}
 	
 	@GetMapping("/jenkins")
+	@ResponseBody
 	public String getMsg(){
-		return "jenkins";
+		return "hello jenkins";
 	}
 }
